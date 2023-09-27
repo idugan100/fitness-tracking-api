15876b8a079bed5e507a948b11e8782800372611
idugan100 <isaacd4444@gmail.com>
2023-09-27 10:30:50 -0400

    created route group for lifts routes

--- server.go
+++ server.go
@@ -11,10 +11,14 @@ func main() {
 	controllers.Connect_to_database()
 	server := gin.Default()
 
-	server.GET("lift/:id", controllers.GetLift)
-	server.GET("lifts", controllers.GetAllLifts)
-	server.GET("lifts/search", controllers.SearchLiftsByName)
-	server.DELETE("/lifts/delete/:id", controllers.DeleteLift)
+	lift_group := server.Group("/lifts")
+	{
+		lift_group.GET("/:id", controllers.GetLift)
+		lift_group.GET("", controllers.GetAllLifts)
+		lift_group.GET("/search", controllers.SearchLiftsByName)
+		lift_group.DELETE("/delete/:id", controllers.DeleteLift)
+	}
+
 	server.Run(":8080")
 
 }
