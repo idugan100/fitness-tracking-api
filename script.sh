41257aa4471b39a32ca224fde1988478715ddeab
idugan100 <isaacd4444@gmail.com>
2023-10-19 11:22:21 -0400

    history rewrite

---
+++ script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git filter-branch --env-filter '
+OLD_EMAIL="isaacdugan@Isaacs-MacBook-Pro.local"
+CORRECT_NAME="idugan100"
+CORRECT_EMAIL="isaacd4444@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
