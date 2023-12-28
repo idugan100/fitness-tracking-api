d2992524f1bc5b41957decf9f4a05a4ba024570d
idugan100 <isaacd4444@gmail.com>
2023-12-28 02:22:40 -0500

    added documentation page that is served from backend

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fitness-tracker-api/testbackend/controllers"
 	"fitness-tracker-api/testbackend/database"
 	"log"
+	"text/template"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
@@ -19,6 +20,7 @@ func main() {
 	server := gin.Default()
 	server.ForwardedByClientIP = true
 	server.SetTrustedProxies([]string{"127.0.0.1"})
+	// server.LoadHTMLGlob("./templates/*")
 
 	liftController := &controllers.LiftController{DB: db}
 	cardioController := &controllers.CardioController{DB: db}
@@ -49,6 +51,21 @@ func main() {
 		workout_group.DELETE("/:id", workoutController.DeleteWorkout)
 		workout_group.POST("", workoutController.AddWorkout)
 	}
+	server.GET("/documentation", func(ctx *gin.Context) {
+		tmp, err := template.ParseFiles("./templates/documentation.tmpl")
+		if err != nil {
+			ctx.AbortWithStatus(500)
+			log.Print(err)
+			return
+		}
+		err = tmp.Execute(ctx.Writer, nil)
+		// ctx.HTML(http.StatusOK, "documentation.tmpl", nil)
+		if err != nil {
+			ctx.AbortWithStatus(500)
+			log.Print(err)
+			return
+		}
+	})
 
 	server.Run(":8080")
 
