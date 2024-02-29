0c21b1a4b72c99bb93b7553d2bb748232ddc0a3e
idugan100 <isaacd4444@gmail.com>
2024-02-28 21:59:24 -0500

    implementing get all workout logs route

--- controllers/liftinglog_controller.go
+++ controllers/liftinglog_controller.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"database/sql"
+	"fitness-tracker-api/testbackend/models"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,22 +12,42 @@ type LiftingLogController struct {
 	DB *sql.DB
 }
 
-func (*LiftingLogController) GetAllWorkoutLogs(ctx *gin.Context) {
+func (lc *LiftingLogController) GetAllWorkoutLogs(ctx *gin.Context) {
+	rows, err := lc.DB.Query("SELECT * FROM LiftingLog")
+	if err != nil {
+		log.Print(err)
+		ctx.AbortWithStatus(500)
+		return
+	}
+	defer rows.Close()
+
+	var lifting_log_list []models.LiftingLog
+
+	for rows.Next() {
+		var lifting_log models.LiftingLog
+		err = rows.Scan(&lifting_log.Id, &lifting_log.LiftId, &lifting_log.Weight, &lifting_log.Sets, &lifting_log.Reps, &lifting_log.WorkoutId)
+		if err != nil {
+			log.Print(err)
+			ctx.AbortWithStatus(500)
+			return
+		}
+		lifting_log_list = append(lifting_log_list, lifting_log)
+	}
+	ctx.JSON(200, lifting_log_list)
+}
+
+func (lc *LiftingLogController) GetWorkoutLogById(ctx *gin.Context) {
 	ctx.JSON(200, "hello")
 }
 
-func (*LiftingLogController) GetWorkoutLogById(ctx *gin.Context) {
+func (lc *LiftingLogController) WorkoutLogsByWorkout(ctx *gin.Context) {
 	ctx.JSON(200, "hello")
 }
 
-func (*LiftingLogController) WorkoutLogsByWorkout(ctx *gin.Context) {
+func (lc *LiftingLogController) DeleteWorkoutLog(ctx *gin.Context) {
 	ctx.JSON(200, "hello")
 }
 
-func (*LiftingLogController) DeleteWorkoutLog(ctx *gin.Context) {
-	ctx.JSON(200, "hello")
-}
-
-func (*LiftingLogController) AddWorkoutLog(ctx *gin.Context) {
+func (lc *LiftingLogController) AddWorkoutLog(ctx *gin.Context) {
 	ctx.JSON(200, "hello")
 }
