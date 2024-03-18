2b21f40641c60e424052c530fa66e1ee4abbac32
idugan100 <isaacd4444@gmail.com>
2024-03-17 23:05:40 -0400

    adding cardio log get all method

--- controllers/cardiolog_controller.go
+++ controllers/cardiolog_controller.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"database/sql"
+	"fitness-tracker-api/testbackend/models"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +18,26 @@ func NewCardioLogController(DB *sql.DB) *CardioLogController {
 }
 
 func (c *CardioLogController) GetAllCardioLogs(ctx *gin.Context) {
-	ctx.JSON(200, "hi")
+	rows, err := c.DB.Query("SELECT * FROM CardioLog")
+	if err != nil {
+		log.Print(err)
+		ctx.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+	var cardioLogList []models.CardioLog
+	for rows.Next() {
+		var cardioLog models.CardioLog
+		err := rows.Scan(&cardioLog.Id, &cardioLog.CardioId, &cardioLog.Time, &cardioLog.Distance, &cardioLog.WorkoutId)
+		if err != nil {
+			log.Print(err)
+			ctx.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		cardioLogList = append(cardioLogList, cardioLog)
+	}
+
+	ctx.JSON(http.StatusOK, cardioLogList)
 }
 
 func (c *CardioLogController) GetCardioLogById(ctx *gin.Context) {
