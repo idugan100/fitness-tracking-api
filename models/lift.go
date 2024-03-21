c42b0a0b0e50752e1c6b012dbad5be4b374b38d9
idugan100 <isaacd4444@gmail.com>
2024-03-20 23:25:45 -0400

    switching lift struct to *bool due to validators issue

--- models/lift.go
+++ models/lift.go
@@ -1,9 +1,10 @@
 package models
 
+// using *bool due to issue with gin validators here: https://github.com/gin-gonic/gin/issues/814#issuecomment-294636138
 type Lift struct {
 	Id       int    `json:"id"`
 	Name     string `json:"name" binding:"required"`
-	Compound bool   `json:"compound" binding:"required"`
-	Upper    bool   `json:"upper" binding:"required"`
-	Lower    bool   `json:"lower" binding:"required"`
+	Compound *bool  `json:"compound" binding:"required"`
+	Upper    *bool  `json:"upper" binding:"required"`
+	Lower    *bool  `json:"lower" binding:"required"`
 }
